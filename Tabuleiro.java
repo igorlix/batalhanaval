1f1dbaf4e4ac410c8e2fb22d92c8c20894fc3bea
Pietra Rodrigues <104863366+pietrarodriguesbaia@users.noreply.github.com>
2022-12-21 10:58:07 -0300

    Retorno da grelha do jogo

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -12,7 +12,20 @@ public class Tabuleiro {
         tabuleiro[linha][coluna]=0; 
     }
     public static void mostrarTabuleiro (int [][] tabuleiro) {
-
+        System.out.println("\t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8 \t9 \t10");
+        for (int linha = 0; linha < 10; linha ++) {
+            System.out.print((linha+1)+" "); 
+            for (int coluna = 0; coluna < 10; coluna++) {
+                if (tabuleiro[linha][coluna] == 0) {
+                    System.out.print("\t" + "~");
+                } else if (tabuleiro[linha][coluna] == -1) {
+                    System.out.print("\t" + "•"); 
+                } else if (tabuleiro[linha][coluna] == 1) {
+                    System.out.print("\t" + "X"); 
+                } 
+            }
+            System.out.println(); 
+        }
     }
     public static void alterarTabuleiro () {
 
